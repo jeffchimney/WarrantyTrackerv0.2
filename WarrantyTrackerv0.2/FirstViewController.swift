d905083e2970e9e03e516726b40f53c775d6a240
Jeff Chimney <jeff.chimney@avssystems.ca>
2016-11-01 12:17:40 -0700

    Initial Commit

---
+++ WarrantyTrackerv0.2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WarrantyTrackerv0.2
+//
+//  Created by Jeff Chimney on 2016-11-01.
+//  Copyright © 2016 Jeff Chimney. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WarrantyTrackerv0.2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WarrantyTrackerv0.2
+//
+//  Created by Jeff Chimney on 2016-11-01.
+//  Copyright © 2016 Jeff Chimney. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
