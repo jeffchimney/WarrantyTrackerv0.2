583d0b4ff4b33a9c4550b6318c91342ee719b1d5
Jeff Chimney <jeff.chimney@avssystems.ca>
2016-11-29 16:01:17 -0800

    Not liking how the sectioned table view is going

--- WarrantyTrackerv0.2/PrimaryViewController.swift
+++ WarrantyTrackerv0.2/PrimaryViewController.swift
@@ -37,6 +37,92 @@ class PrimaryViewController: UIViewController, UITableViewDelegate, UITableViewD
         // sorted by recent by default
         sortBySegmentControl.selectedSegmentIndex = 0
         
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let recordEntity = NSEntityDescription.entity(forEntityName: "Record", in: managedContext)!
+        
+        let record = NSManagedObject(entity: recordEntity, insertInto: managedContext) as! Record
+        let record2 = NSManagedObject(entity: recordEntity, insertInto: managedContext) as! Record
+        let record3 = NSManagedObject(entity: recordEntity, insertInto: managedContext) as! Record
+        let record4 = NSManagedObject(entity: recordEntity, insertInto: managedContext) as! Record
+        let record5 = NSManagedObject(entity: recordEntity, insertInto: managedContext) as! Record
+        
+        record.title = "Test"
+        record.descriptionString = "Test"
+        //record.tags = tagArray
+        record.warrantyStarts = NSDate()
+        record.warrantyEnds = NSDate()
+        record.itemImage = NSData()
+        record.receiptImage = NSData()
+        record.weeksBeforeReminder = 2
+        record.hasWarranty = true
+        record.dateCreated = Date() as NSDate?
+        record.recentlyDeleted = false
+        record.expired = false
+        
+        record2.title = "Test2"
+        record2.descriptionString = "Test2"
+        //record.tags = tagArray
+        record2.warrantyStarts = NSDate()
+        record2.warrantyEnds = NSDate()
+        record2.itemImage = NSData()
+        record2.receiptImage = NSData()
+        record2.weeksBeforeReminder = 2
+        record2.hasWarranty = true
+        record2.dateCreated = Date() as NSDate?
+        record2.recentlyDeleted = false
+        record2.expired = false
+        
+        record3.title = "Test3"
+        record3.descriptionString = "Test3"
+        //record.tags = tagArray
+        record3.warrantyStarts = NSDate()
+        record3.warrantyEnds = NSDate()
+        record3.itemImage = NSData()
+        record3.receiptImage = NSData()
+        record3.weeksBeforeReminder = 2
+        record3.hasWarranty = true
+        record3.dateCreated = Date() as NSDate?
+        record3.recentlyDeleted = false
+        record3.expired = false
+        
+        record4.title = "Test4"
+        record4.descriptionString = "Test4"
+        //reco4d.tags = tagArray
+        record4.warrantyStarts = NSDate()
+        record4.warrantyEnds = NSDate()
+        record4.itemImage = NSData()
+        record4.receiptImage = NSData()
+        record4.weeksBeforeReminder = 2
+        record4.hasWarranty = true
+        record4.dateCreated = Date() as NSDate?
+        record4.recentlyDeleted = false
+        record4.expired = false
+        
+        record5.title = "Test5"
+        record5.descriptionString = "Test5"
+        //record.tags = tagArray
+        record5.warrantyStarts = NSDate()
+        record5.warrantyEnds = NSDate()
+        record5.itemImage = NSData()
+        record5.receiptImage = NSData()
+        record5.weeksBeforeReminder = 2
+        record5.hasWarranty = true
+        record5.dateCreated = Date() as NSDate?
+        record5.recentlyDeleted = false
+        record5.expired = false
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
         self.warrantiesTableView.reloadData()
         
         searchController.dimsBackgroundDuringPresentation = false
@@ -137,9 +223,10 @@ class PrimaryViewController: UIViewController, UITableViewDelegate, UITableViewD
         dateFormatter.dateFormat = "MMM d, yyyy"
         
         if searchActive {
+            sections = [filteredRecords, recentlyDeletedRecords, expiredRecords]
             if sortBySegmentControl.selectedSegmentIndex == 0 {
-                filteredRecords.sort(by:{ $0.dateCreated?.compare($1.dateCreated as! Date) == .orderedDescending})
-                let record = filteredRecords[indexPath.row]
+                sections[indexPath.section].sort(by:{ $0.dateCreated?.compare($1.dateCreated as! Date) == .orderedDescending})
+                let record = sections[indexPath.section][indexPath.row]
                 cell.title.text = record.title
                 cell.descriptionView.text = record.descriptionString
                 cell.warrantyStarts.text = dateFormatter.string(from: record.warrantyStarts! as Date)
@@ -147,8 +234,8 @@ class PrimaryViewController: UIViewController, UITableViewDelegate, UITableViewD
                 let recordImage = UIImage(data: record.itemImage as! Data)
                 cell.warrantyImageView.image = recordImage
             } else {
-                filteredRecords.sort(by:{ $0.warrantyEnds?.compare($1.warrantyEnds as! Date) == .orderedAscending})
-                let record = filteredRecords[indexPath.row]
+                sections[indexPath.section].sort(by:{ $0.warrantyEnds?.compare($1.warrantyEnds as! Date) == .orderedAscending})
+                let record = sections[indexPath.section][indexPath.row]
                 cell.title.text = record.title
                 cell.descriptionView.text = record.descriptionString
                 cell.warrantyStarts.text = dateFormatter.string(from: record.warrantyStarts! as Date)
@@ -157,6 +244,7 @@ class PrimaryViewController: UIViewController, UITableViewDelegate, UITableViewD
                 cell.warrantyImageView.image = recordImage
             }
         } else {
+            sections = [records, recentlyDeletedRecords, expiredRecords]
             if sortBySegmentControl.selectedSegmentIndex == 0 {
                 sections[indexPath.section].sort(by:{ $0.dateCreated?.compare($1.dateCreated as! Date) == .orderedDescending})
                 let record = sections[indexPath.section][indexPath.row]
@@ -185,8 +273,11 @@ class PrimaryViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if searchActive {
-            return filteredRecords.count
+            sections = [filteredRecords, recentlyDeletedRecords, expiredRecords]
+            return sections[section].count
         } else {
+            sections = [records, recentlyDeletedRecords, expiredRecords]
+            print(sections[section].count)
             return sections[section].count
         }
     }
@@ -194,16 +285,27 @@ class PrimaryViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             
+            warrantiesTableView.beginUpdates()
+            
             if searchActive {
-                let recordToRemove = filteredRecords[indexPath.row]
+                sections = [filteredRecords, recentlyDeletedRecords, expiredRecords]
+                let recordToRemove = sections[indexPath.section][indexPath.row]
                 let index = records.index(of: recordToRemove)
+                delete(record: sections[indexPath.section][indexPath.row])
                 records.remove(at: index!)
             } else {
+                sections = [records, recentlyDeletedRecords, expiredRecords]
+                sections[1].insert(sections[indexPath.section][indexPath.row], at: 0)
+                recentlyDeletedRecords = sections[1]
                 delete(record: sections[indexPath.section][indexPath.row])
                 sections[indexPath.section].remove(at: indexPath.row)
+                records = sections[indexPath.section]
             }
-        
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            warrantiesTableView.deleteRows(at: [indexPath], with: .fade)
+            let toIndexPath = IndexPath(row: 0, section: 1)
+            warrantiesTableView.insertRows(at: [toIndexPath], with: .fade)
+            
+            warrantiesTableView.endUpdates()
         }
     }
     
@@ -241,9 +343,6 @@ class PrimaryViewController: UIViewController, UITableViewDelegate, UITableViewD
                 print("Could not save. \(error), \(error.localizedDescription)")
             }
         }
-
-        sections = [records, recentlyDeletedRecords, expiredRecords]
-        warrantiesTableView.reloadData()
     }
     
     
